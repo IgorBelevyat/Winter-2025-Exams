f48122bebcbbe587a5d934bcaafcc7b9c688bd6f
Igor Belevyat <beliaviat228@gmail.com>
2025-01-13 22:34:34 +0200

    removing rest

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,8 +1,7 @@
 // Refactor following solution
 // Find key by value
 
-const find = (object, ...rest) => {
-  const value = rest.pop(1);
+const find = (object, value) => {
   for (const name in object) {
     if (object[name] === value) {
       return name;
