346a7bcdf293d58f41bcca38df40df2b196aef90
Igor Belevyat <beliaviat228@gmail.com>
2025-01-13 21:35:42 +0200

    adding a strict variable defenition

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,15 @@
 // Refactor following solution
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+const Filter = (T, t) => {
+  let remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
