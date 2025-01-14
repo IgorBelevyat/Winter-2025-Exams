25c8d6fc071698ee291c3e752b4f030a517d5d8b
Igor Belevyat <beliaviat228@gmail.com>
2025-01-15 00:04:18 +0200

    Romove unnecessary parts and variables

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,14 +1,11 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
+invert = (A) => {
+  T = Object.keys(A);
   T.forEach((_, i) => {
     T[i] = A.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+  });
   return T;
 };
 
