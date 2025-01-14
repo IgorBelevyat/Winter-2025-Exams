561f800343b9cb1b7fcce0816273179664613754
Igor Belevyat <beliaviat228@gmail.com>
2025-01-14 23:21:18 +0200

    Add array for results. Chand the method of function declaration

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,7 +1,8 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
+const intersection = (object_1, object_2) => {
+  const result = {};
   first_keys = Object.keys(object_1);
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
@@ -9,8 +10,6 @@ intersection = function intersection(object_1, object_2) {
     } else {
       delete object_1[attribute_name];
     }
-    
-    
   }
   return object_1;
 };
