06952c6813f8128ed2f29f5726e981f437e38bae
Igor Belevyat <beliaviat228@gmail.com>
2025-01-15 21:44:25 +0200

    Remove else

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -4,15 +4,16 @@
 const Parseip = (input) => {
   const result = [];
   if (input === '') return;
-  else {
-    const parts = input.split('.');
-    if (parts.length != 4) return;
-    for (const part of parts) {
-      const num = parseInt(part);
-      if (isNaN(num)) return;
-      result.push (num)
-    }
+
+  const parts = input.split('.');
+  if (parts.length != 4) return;
+
+  for (const part of parts) {
+    const num = parseInt(part);
+    if (isNaN(num)) return;
+    result.push (num)
   }
+  
   return result;
 };
 
