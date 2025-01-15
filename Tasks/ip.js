a983e54ae5e0dc6017480e640c03feb9aaf2fe4f
Igor Belevyat <beliaviat228@gmail.com>
2025-01-15 21:49:17 +0200

    Change the way of checking empty spaces and if the parts were inputed correctly

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,17 +3,17 @@
 
 const Parseip = (input) => {
   const result = [];
-  if (input === '') return;
+  if (!input) return;
 
   const parts = input.split('.');
   if (parts.length != 4) return;
 
   for (const part of parts) {
-    const num = parseInt(part);
-    if (isNaN(num)) return;
+    const num = parseInt(part, 10);
+    if (isNaN(num) || num <0 || num > 255) return;
     result.push (num)
   }
-  
+
   return result;
 };
 
